278e4ae1c1f88a7fa3a26a647c36a8db4fcb8485
Daniil <korostindi@mail.ru>
2023-03-18 23:56:00 +0400

    Задачки

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = number / 10000;
+int num2 = number / 1000 % 10;
+int num5 = number % 10;
+int num4 = number % 100 / 10;
+
+
+if (num1 == num5 && num2 == num4)
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+Console.WriteLine("Enter coordinates for point A, ");
+Console.Write("x: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinates for point B, ");
+Console.Write("x: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double DistanceIn3D(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) +
+    (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+}
+
+double result = DistanceIn3D(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Distance between two points is {Math.Round(result, 2)}");
+
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите  число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
